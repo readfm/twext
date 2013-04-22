39dc657deb3f145dbec80752807be9720cab0542
wafaa <wafaa.abd.elsalam@gmail.com>
2013-04-22 18:23:34 +0200

    Adding languages to languages.js

--- scooch/scooch/js/languages.js
+++ scooch/scooch/js/languages.js
@@ -1,35 +1,38 @@
+// Unicode escape characters used
+// "http://en.wikipedia.org/wiki/List_of_Unicode_characters"
+// "http://www.mobilefish.com/services/unicode_escape_sequence_converter/unicode_escape_sequence_converter.php"
 /**
 * Key/Value bbject contains languages info(names and codes).
 * To add a new language, you have to add its code and its name to the object ("english":"en")
 * To delete a langauge, you have to delete its code and its name from the object ("english":"en")
 */
 var languages = {
-  "fran\u00E7ais": "fr",  // français
-  "italiano": "it",
-  "espa\u0148ol": "es", // español
-  "english": "en",
-  "portugu\u00eas": "pt", // português
-  "\u010de\u0161tina": "cs", // ceština
-  "latvie\u0161u valoda": "lv", // latviešu valoda
-  "lietuvi\u0173 kalba": "lt",  // lietuviu kalba
-  "dansk": "da",
-  "nederlands": "nl",
-  "polszczyzna": "pl",
-  "eesti": "et",
-  "suomi": "fi",
-  "limba rom\u00e2n\u0103": "ro", // limba româna
+  "fran\u00E7ais": "fr",  // FRENCH
+  "italiano": "it", // ITALIAN
+  "espa\u0148ol": "es", // SPANISH
+  "english": "en",  // ENGLISH
+  "portugu\u00eas": "pt", // PORTUGUESE
+  "\u010de\u0161tina": "cs", // CZECH
+  "latvie\u0161u valoda": "lv", // LATVIAN
+  "lietuvi\u0173 kalba": "lt",  // LITHUANIAN
+  "dansk": "da",  // DANISH
+  "nederlands": "nl", // DUTCH
+  "polszczyzna": "pl",  // POLISH
+  "eesti": "et",  // ESTONIAN
+  "suomi": "fi",  // FINNISH
+  "limba rom\u00e2n\u0103": "ro", // ROMANIAN
   "sloven\u010dina": "sk",  // SLOVAK
   "sloven\u0161\u010Dina": "sl",  // SLOVINIAN
-  "magyar": "hu",
-  "svenska": "sv",
-  "deutsch": "de",
+  "magyar": "hu", // HUNGARIAN
+  "svenska": "sv",  // SWEDISH
+  "deutsch": "de",  // GERMAN
   "catal\u00E0": "ca",  // CATALAN
   "t\u00FCrk\u00E7e": "tr", // TURKISH
   "\u0411\u044a\u043b\u0491apc\u043a\u0438 e\u0437\u0438\u043a": "bg",  // BULGARIAN
   "\u03b5\u03bb\u03bb\u03b7\u03bd\u03b9\u03ba\u03ac": "el", // GREEK
-  "pycc\u043A\u0438\u0439 \u044F\u0437\u044B\u043A": "ru"//, // RUSSIAN
-  //"\u4e2d\u6587": "zh-CN"//,  // CHINESE
-  //"\u0E4x\u0E1x\u0E2x": "th"  // THAI
+  "pycc\u043A\u0438\u0439 \u044F\u0437\u044B\u043A": "ru", // RUSSIAN
+  "\u4e2d\u6587": "zh-CN",  // CHINESE
+  "\u0e44\u0e17\u0e22": "th"  // THAI
 };
 
 var languages_codes = languagesCodes(); // languages codes array
